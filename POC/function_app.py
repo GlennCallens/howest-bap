9efe5b22f75e917675d782e226c7ef52c021a8cb
glenn.callens <glenn.callens@outlook.com>
2024-05-12 18:30:36 +0200

    Add post solution

--- POC/function_app.py
+++ POC/function_app.py
@@ -7,7 +7,10 @@ from dotenv import load_dotenv
 
 from ai.import_data import import_repo
 from github.download_repo import download_repo
+from github.issue import get_issue, post_comment_with_solution
 from github.scan_issues import scan
+from ai.get_context import search
+from ai.get_solution import get_solution, decide_file
 
 load_dotenv('.env')
 app = func.FunctionApp()
@@ -58,5 +61,31 @@ def scan_projects(req: func.HttpRequest) -> func.HttpResponse:
     scan(repo)
     return func.HttpResponse(
         "Scan successfully",
+        status_code=200
+    )
+
+
+@app.route(route="get_solution")
+def get_solution(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request for get solution.')
+
+    repo = req.params.get('repo')
+    issue_number = req.params.get('issue_number')
+
+    if not repo or not issue_number:
+        return func.HttpResponse(
+            "Invalid Request: Get solution needs 2 arguments: repository name and issue number",
+            status_code=400
+        )
+    question = get_issue(repo, issue_number)
+
+    context = search(question, repo)
+    file = decide_file(question, context)
+    solution = get_solution(question, context[file])
+
+    post_comment_with_solution(repo, issue_number, solution)
+
+    return func.HttpResponse(
+        "Solution added.",
         status_code=200
     )
---
+++ POC/github/issue.py
@@ -0,0 +1,56 @@
+import json
+import logging
+import os
+
+import requests
+
+from github.authentication import authenticate
+
+
+def get_issue(repo: str, issue_number: str):
+    token = authenticate(os.getenv('CERT_PATH'), os.getenv('GITHUB_APP_ID'), os.getenv('GITHUB_API_URL'))
+
+    payload = {}
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': 'Bearer ' + str(token),
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+
+    response = requests.request("GET",
+                                os.getenv(
+                                    'GITHUB_API_URL') + "/repos/glenncallens/" + repo + f"/issues/{issue_number}",
+                                headers=headers,
+                                data=payload)
+
+    if response.status_code == 200:
+        json_response = response.json()
+        return json_response['title'] + " " + json_response['body']
+    else:
+        logging.error(f"Failed to get issue  HTTP Status Code: {response.status_code}")
+        return []
+
+
+def post_comment_with_solution(repo: str, issue_number: str, comment: str):
+    token = authenticate(os.getenv('CERT_PATH'), os.getenv('GITHUB_APP_ID'), os.getenv('GITHUB_API_URL'))
+
+    payload = json.dumps({
+        "body": comment
+    })
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': 'Bearer ' + str(token),
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+
+    response = requests.request("POST",
+                                os.getenv(
+                                    'GITHUB_API_URL') + "/repos/glenncallens/" + repo + f"/issues/{issue_number}/comments",
+                                headers=headers,
+                                data=payload)
+
+    if response.status_code == 201:
+        return True
+    else:
+        logging.error(f"Failed to post comment  HTTP Status Code: {response.status_code}")
+        return False
